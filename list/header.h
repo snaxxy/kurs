50cf124198bd81c50ced8048bf24784950ea2aa0
alex <snaxxy72@gmail.com>
2016-04-18 20:28:05 +0300

    Initial commit

---
+++ list/header.h
@@ -0,0 +1,68 @@
+#ifndef listH
+#define listH 1
+
+#include <iostream>
+
+//Структура, описывающая элемент списка
+struct item
+{
+	static int summ; //Счётик элементов
+		
+	item *previous; //Указатель на предыдущий элемент
+	double info; //Поле данных
+	item *next; //Указатель на следующий элемент
+};
+
+//Абстрактный базовый класс
+class list : public item //Наследует структуру item
+{
+public:
+	item *first_item = NULL; //Указатель на первый элемент
+	item *last_item = NULL; //Указатель на последний элемент
+
+	//Методы
+	virtual void add(double data = NULL); 
+	virtual void del() = 0; //Виртуальная функция
+
+	void display();
+	void change(int key = NULL);
+	void search(double key = NULL);
+	void sort(double key = NULL);
+};
+
+//Класс, описывающий тип списка stack
+class stack : virtual public list //Виртуально наследует абстрактный класс list
+{
+public:
+	stack(double data = NULL); //Конструктор
+
+	void del(); //Метод
+
+	~stack(){} //Детруктор
+};
+
+//Класс, описывающий тип списка turn
+class turn : virtual public list //Виртуально наследует абстрактный класс list
+{
+public:
+	turn(double data = NULL); //Конструктор
+	
+	void del(); //Метод
+
+	~turn(){} //Детруктор
+};
+
+//Класс, описывающий тип списка deck
+class deck : public stack, public turn //Наследует классы stack и turn
+{
+public:
+	deck(double data = NULL); //Конструктор
+
+	//Методы
+	void add(double data = NULL);
+	void del();
+
+	~deck(){} //Детруктор
+};
+
+#endif
---
+++ list/stack.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+#include "header.h"
+
+using namespace std;
+
+//Конструктор
+stack::stack(double data)
+{
+	previous = NULL;
+	info = data;
+	next = NULL;
+}
+
+//Функция удаления элемента из списка
+void stack::del()
+{
+	if (first_item) //Проверка на существование списка
+	{
+		if (first_item->next) this->next = first_item->next; //Проверка на существование второго элемента и присвоение указателю объекта на следующий элемент адрес первого
+		else this->next = NULL; //Присвоение указателю объекта на следующий элемент NULL
+
+		//delete first_item;
+		first_item = this->next; //Присвоение указателю на первый элемент адрес нового первого
+
+		summ--; //Уменьшени счётчика элементов на один
+	}
+	else cout << "Список пуст.\n";
+}
+
+/*
+stack::~stack()
+{
+	return;
+	
+	delete[] first_item;
+	delete[] last_item;
+
+	return;
+
+	item *temp = last_item->previous;
+
+	for (int i = 1; i <= summ; i++)
+	{
+		temp->next->next = NULL;
+		temp->next->previous = NULL;
+
+		temp = temp->previous;
+	}
+}
+*/
